d1774d5cbefc894183e91535251c28a5437202bb
Nicholas Hardy <nicholas.h.hardy@gmail.com>
2023-09-29 22:09:49 +1000

    fix imports

--- src/test/java/org/apache/commons/collections4/collection/IndexedCollectionTest.java
+++ src/test/java/org/apache/commons/collections4/collection/IndexedCollectionTest.java
@@ -18,17 +18,18 @@ package org.apache.commons.collections4.collection;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.apache.commons.collections4.TestUtils;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.TransformerUtils;
 import org.apache.commons.collections4.Unmodifiable;
 import org.junit.jupiter.api.Test;
 
