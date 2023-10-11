37a5e086cc7a4fcebb67e13dc61f03fb08461718
Nicholas Hardy <nicholas.h.hardy@gmail.com>
2023-10-12 05:46:25 +1100

    update junit imports

--- src/test/java/org/apache/commons/collections4/bloomfilter/IndexProducerFromBitmapProducerTest.java
+++ src/test/java/org/apache/commons/collections4/bloomfilter/IndexProducerFromBitmapProducerTest.java
@@ -16,13 +16,14 @@
  */
 package org.apache.commons.collections4.bloomfilter;
 
-import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.LongPredicate;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IndexProducerFromBitmapProducerTest extends AbstractIndexProducerTest {
 
